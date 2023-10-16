f5b12426cb62363b94ce7d2d7b94d295ff61d0bc
Tomich37 <Tomich7372@gmail.com>
2023-10-16 17:16:15 +0700

    изъятие данных опросника из поста по ссылке

--- app/tg_bot.py
+++ app/tg_bot.py
@@ -2,6 +2,7 @@ import telebot
 from app.set_logs import SetLogs
 import configparser
 from app.vk_bot import VKBot
+import requests
 
 
 class TgBot:
@@ -24,22 +25,39 @@ class TgBot:
         def start(message):
             self.tg_bot.send_message(message.chat.id, 'Добрый день, для получения суммарных результатов голосований прошу отправить эксель файл с ссылками на записи с голосованями')
 
+        @self.tg_bot.message_handler(content_types=['document'])
+        def handle_document(message):
+            user_id = message.from_user.id
+            try:
+                # Получаем информацию о загруженном документе
+                file_info = self.tg_bot.get_file(message.document.file_id)
+                file_path = f"https://api.telegram.org/file/bot{self.tg_token}/{file_info.file_path}"
 
-        # Обработчик команды /get_results
-        @self.tg_bot.message_handler(commands=['get_results'])
-        def get_results(message):
-            links = self.read_links_from_excel('your_excel_file.xlsx')
-            total_results = []
+                # Создаем временный файл для сохранения документа
+                temp_file_path = 'temp.xlsx'
 
-            for link in links:
-                poll_results = self.get_poll_results(link, self.vk_token)
-                # Обработка результатов голосования, например, суммирование
-                # Вам нужно подставить свою логику обработки результатов
-                
-                total_results.append(poll_results)
+                # Загружаем документ
+                response = requests.get(file_path)
+                with open(temp_file_path, 'wb') as temp_file:
+                    temp_file.write(response.content)
 
-            # Отправляем результаты в Telegram
-            self.tg_bot.reply_to(message, f'Total results: {total_results}')
+                # Читаем ссылки из загруженного эксель-файла
+                links = self.vk_bot.read_links_from_excel(temp_file_path)
+                total_results = []
+
+                for link in links:
+                    poll_results = self.vk_bot.get_poll_results(link, self.vk_token)
+                    # Обработка результатов голосования, например, суммирование
+                    # Вам нужно подставить свою логику обработки результатов
+
+                    total_results.append(poll_results)
+
+                # Отправляем результаты в Telegram
+                self.tg_bot.reply_to(message, f'Общие результаты: {total_results}')
+            except Exception as e:
+                print(f"An error occurred in tg_bot/handle_document: {e}")
+                self.logger.exception(f"An error occurred in tg_bot/handle_document: {e}")
+                self.tg_bot.send_message(user_id, "Произошла ошибка при загрузке документа. Пожалуйста, повторите попытку позже.")
 
         # Запуск бота
         self.tg_bot.polling()
--- app/vk_bot.py
+++ app/vk_bot.py
@@ -1,4 +1,4 @@
-import requests
+import requests, re
 from openpyxl import load_workbook
 
 class VKBot:
@@ -8,22 +8,58 @@ class VKBot:
 
     # Функция для получения результатов голосования по ссылке
     def get_poll_results(self, poll_url, vk_token):
-        poll_id = poll_url.split('/')[-1]
-        url = 'https://api.vk.com/method/polls.getById'
-        params = {
-            'owner_id': -1,
-            'poll_id': poll_id,
-            'access_token': vk_token,
-            'v': 5.221
-        }
-        response = requests.get(url, params=params)
-        poll_results = response.json()
-        return poll_results
+        try:
+            post_id = self.get_post_id(poll_url)
+            owner_id, post_id = post_id.split('_')
+            url = 'https://api.vk.com/method/wall.getById'
+            params = {
+                'posts': f'-{owner_id}_{post_id}',
+                'access_token': vk_token,
+                'v': 5.221
+            }
+            response = requests.get(url, params=params)
+            post_results = response.json()
+            self.extract_poll_attachments(post_results) 
+        except Exception as e:
+            print(f"An error occurred in vk_bot/get_poll_results: {e}")
+            self.logger.exception(f"An error occurred in vk_bot/get_poll_results: {e}")
 
     # Функция для чтения ссылок из Excel файла
     def read_links_from_excel(self, file_path):
-        wb = load_workbook(filename=file_path)
-        sheet = wb.active
-        links = [cell.value for cell in sheet['A'] if cell.value is not None]
-        return links
-    
+        try:
+            wb = load_workbook(filename=file_path)
+            sheet = wb.active
+            links = [cell.value for cell in sheet['A'] if cell.value is not None]
+            print(f'read_links_from_excel: {links}')
+            return links
+        except Exception as e:
+            print(f"An error occurred in vk_bot/read_links_from_excel: {e}")
+            self.logger.exception(f"An error occurred in vk_bot/read_links_from_excel: {e}")
+        
+    def get_post_id(self, url):
+        match = re.search(r'wall-(\d+_\d+)', url)
+        if match:
+            return match.group(1)
+        else:
+            return None
+        
+    def extract_poll_attachments(self, json_response):
+        try:
+            poll_attachments = []
+
+            items = json_response.get('response', {}).get('items', [])
+            for item in items:
+                attachments = item.get('attachments', [])
+                for attachment in attachments:
+                    if 'poll' in attachment:
+                        poll = attachment.get('poll', {})
+                        question = poll.get('question', '')
+                        answers = poll.get('answers', [])
+
+                        poll_attachments.append({'question': question, 'answers': answers})
+            
+            print(poll_attachments)
+
+        except Exception as e:
+            print(f"An error occurred in vk_bot/extract_poll_attachments: {e}")
+            self.logger.exception(f"An error occurred in vk_bot/extract_poll_attachments: {e}")
