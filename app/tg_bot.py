c0f9e9040a45159bf68b5e102d81af002ec036dd
Tomich37 <Tomich7372@gmail.com>
2023-10-18 17:37:39 +0700

    Объединение результатов в один, вывод ответа

--- app/tg_bot.py
+++ app/tg_bot.py
@@ -2,7 +2,7 @@ import telebot
 from app.set_logs import SetLogs
 import configparser
 from app.vk_bot import VKBot
-import requests
+import requests, re
 
 
 class TgBot:
@@ -28,36 +28,109 @@ class TgBot:
         @self.tg_bot.message_handler(content_types=['document'])
         def handle_document(message):
             user_id = message.from_user.id
-            try:
-                # Получаем информацию о загруженном документе
-                file_info = self.tg_bot.get_file(message.document.file_id)
-                file_path = f"https://api.telegram.org/file/bot{self.tg_token}/{file_info.file_path}"
+            self.tg_bot.send_message(message.chat.id, 'Идет обработка документа, ожидайте ее завершения')
+            # Проверяем расширение файла
+            if message.document.file_name.endswith('.xlsx'):
+                try:
+                    # Получаем информацию о загруженном документе
+                    file_info = self.tg_bot.get_file(message.document.file_id)
+                    file_path = f"https://api.telegram.org/file/bot{self.tg_token}/{file_info.file_path}"
 
-                # Создаем временный файл для сохранения документа
-                temp_file_path = 'temp.xlsx'
+                    # Создаем временный файл для сохранения документа
+                    temp_file_path = 'temp.xlsx'
 
-                # Загружаем документ
-                response = requests.get(file_path)
-                with open(temp_file_path, 'wb') as temp_file:
-                    temp_file.write(response.content)
+                    # Загружаем документ
+                    response = requests.get(file_path)
+                    with open(temp_file_path, 'wb') as temp_file:
+                        temp_file.write(response.content)
 
-                # Читаем ссылки из загруженного эксель-файла
-                links = self.vk_bot.read_links_from_excel(temp_file_path)
-                total_results = []
+                    # Читаем ссылки из загруженного эксель-файла
+                    links = self.vk_bot.read_links_from_excel(temp_file_path)
+                    number_of_links = len(links)
+                    links = [link for link in links if 'vk.com' in link and 'wall' in link]
+                    number_of_good_links = len(links)
+                    n_links = 0
+                    process_messge = self.tg_bot.send_message(message.chat.id, f'Обработано: {n_links} из {number_of_good_links}')
 
-                for link in links:
-                    poll_results = self.vk_bot.get_poll_results(link, self.vk_token)
-                    # Обработка результатов голосования, например, суммирование
-                    # Вам нужно подставить свою логику обработки результатов
+                    chunk_size = 100
+                    link_chunks = [links[i:i+chunk_size] for i in range(0, len(links), chunk_size)]
 
-                    total_results.append(poll_results)
+                    total_results = {}
 
-                # Отправляем результаты в Telegram
-                self.tg_bot.reply_to(message, f'Общие результаты: {total_results}')
-            except Exception as e:
-                print(f"An error occurred in tg_bot/handle_document: {e}")
-                self.logger.exception(f"An error occurred in tg_bot/handle_document: {e}")
-                self.tg_bot.send_message(user_id, "Произошла ошибка при загрузке документа. Пожалуйста, повторите попытку позже.")
+                    for link_chunk in link_chunks:
+                        poll_results = []
+
+                        for link in link_chunk:
+                            link_id = re.search(r'wall-(\d+_\d+)', link).group(1)
+                            link_id = f'-{link_id}'  # добавляем "-" перед id
+                            poll_results.append(link_id)  # Используем append для добавления id
+                                
+                        poll_results_str = ', '.join(poll_results)
+                        results = self.vk_bot.get_poll_results(poll_results_str, self.vk_token)
+                        n_links += 100
+                        self.tg_bot.edit_message_text(f"Процесс обработки опросов: {n_links}/{number_of_good_links}", chat_id=message.chat.id, message_id=process_messge.message_id)
+                        if results:
+                            if not total_results:
+                                # Если total_results пуст, записываем вопросы и ответы из первого чанка
+                                total_results = {result['question']: result['answers'] for result in results}
+                            else:
+                                # В противном случае обновляем голоса для уже существующих вопросов и ответов
+                                for result in results:
+                                    question = result['question']
+                                    for answer in result['answers']:
+                                        for existing_answer in total_results.get(question, []):
+                                            if answer['text'] == existing_answer['text']:
+                                                existing_answer['votes'] += answer['votes']
+
+                    total_results = [{'question': question, 'answers': answers} for question, answers in total_results.items()]
+
+                    formatted_results = {}
+
+                    for result in total_results:
+                        question = result['question']
+                        answers = result['answers']
+                        if question not in formatted_results:
+                            formatted_results[question] = {'answers': []}
+                        formatted_results[question]['answers'].extend(answers)
+
+                    for question, data in formatted_results.items():
+                        votes = [answer['votes'] for answer in data['answers']]
+                        total_votes = sum(votes)
+                        data['total_votes'] = total_votes
+
+                    # Создаем список для хранения результатов
+                    combined_results = []
+
+                    # Создаем список для хранения результатов
+                    for result in total_results:
+                        question = result['question']
+                        answers = result['answers']
+                        for i, answer in enumerate(answers):
+                            existing_answer = next((item for item in combined_results if item['index'] == i), None)
+                            if existing_answer:
+                                # Если ответ уже существует, суммируем голоса
+                                existing_answer['votes'] += answer['votes']
+                            else:
+                                # Если ответ новый, добавляем его в combined_results
+                                combined_results.append({'index': i, 'text': answer['text'], 'votes': answer['votes']})
+
+                    # Форматируем результаты для вывода в телеграм
+                    formatted_message = ''
+                    formatted_message += f'Вопрос:\n{question}\n\nОтветы:\n'
+
+                    for data in combined_results:                    
+                        formatted_message += f'{data["text"]} | {data["votes"]} | {round((100 * data["votes"])/total_votes, 2)}%\n'
+
+                    formatted_message += f'\n\nВсего голосов: {total_votes}'
+
+                    # Отправляем результаты в Telegram
+                    self.tg_bot.reply_to(message, f'Общие результаты:\nПолучено ссылок: {number_of_links}\nРабочие ссылки: {number_of_good_links}\n\n{formatted_message}')
+                except Exception as e:
+                    print(f"An error occurred in tg_bot/handle_document: {e}")
+                    self.logger.exception(f"An error occurred in tg_bot/handle_document: {e}")
+                    self.tg_bot.send_message(user_id, "Произошла ошибка при загрузке документа. Пожалуйста, повторите попытку позже.")
+            else:
+                self.tg_bot.send_message(message.chat.id, 'Пожалуйста, загрузите файл в формате Excel (xlsx)')
 
         # Запуск бота
         self.tg_bot.polling()
--- app/vk_bot.py
+++ app/vk_bot.py
@@ -1,4 +1,4 @@
-import requests, re
+import requests, json
 from openpyxl import load_workbook
 
 class VKBot:
@@ -7,19 +7,20 @@ class VKBot:
         self.vk_token = vk_token
 
     # Функция для получения результатов голосования по ссылке
-    def get_poll_results(self, poll_url, vk_token):
+    def get_poll_results(self, poll_results_str, vk_token):
         try:
-            post_id = self.get_post_id(poll_url)
-            owner_id, post_id = post_id.split('_')
             url = 'https://api.vk.com/method/wall.getById'
             params = {
-                'posts': f'-{owner_id}_{post_id}',
+                'posts': poll_results_str,
                 'access_token': vk_token,
                 'v': 5.221
             }
             response = requests.get(url, params=params)
-            post_results = response.json()
-            self.extract_poll_attachments(post_results) 
+            json_response = response.json()
+            # json_data = json.dumps(json_response, separators=(',', ':'), ensure_ascii=False)
+            # print(json_data)
+            results = self.extract_poll_attachments(json_response) 
+            return results
         except Exception as e:
             print(f"An error occurred in vk_bot/get_poll_results: {e}")
             self.logger.exception(f"An error occurred in vk_bot/get_poll_results: {e}")
@@ -30,18 +31,11 @@ class VKBot:
             wb = load_workbook(filename=file_path)
             sheet = wb.active
             links = [cell.value for cell in sheet['A'] if cell.value is not None]
-            print(f'read_links_from_excel: {links}')
+            # print(f'read_links_from_excel: {links}')
             return links
         except Exception as e:
             print(f"An error occurred in vk_bot/read_links_from_excel: {e}")
             self.logger.exception(f"An error occurred in vk_bot/read_links_from_excel: {e}")
-        
-    def get_post_id(self, url):
-        match = re.search(r'wall-(\d+_\d+)', url)
-        if match:
-            return match.group(1)
-        else:
-            return None
         
     def extract_poll_attachments(self, json_response):
         try:
@@ -56,10 +50,17 @@ class VKBot:
                         question = poll.get('question', '')
                         answers = poll.get('answers', [])
 
-                        poll_attachments.append({'question': question, 'answers': answers})
-            
-            print(poll_attachments)
-
+                        existing_question = next((item for item in poll_attachments if item['question'] == question), None)
+                        if existing_question:
+                            # Если вопрос уже существует, суммируем голоса в ответах
+                            for i, answer in enumerate(answers):
+                                existing_question['answers'][i]['votes'] += answer['votes']
+                        else:
+                            # Если вопрос новый, добавляем его в poll_attachments
+                            answers_data = [{'text': answer['text'], 'votes': answer['votes']} for answer in answers]
+                            poll_attachments.append({'question': question, 'answers': answers_data})            
+                            
+            return poll_attachments
         except Exception as e:
             print(f"An error occurred in vk_bot/extract_poll_attachments: {e}")
             self.logger.exception(f"An error occurred in vk_bot/extract_poll_attachments: {e}")
